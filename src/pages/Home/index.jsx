3898383fa3fdc64d8598b9525a0226a1d547774d
gustavobtflores <gustavo.thompson7777@gmail.com>
2021-07-24 15:09:46 -0300

    Finishes search input

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
-import Container from './styles';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import { Container, Search } from './styles';
+import logo from '../../assets/logo.svg';
 
-const Home = () => <Container>Hello World!</Container>;
+const Home = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  return (
+    <Container>
+      <Search>
+        <img
+          src={logo}
+          alt="Restaurant Finder"
+          style={{ alignSelf: 'center' }}
+        />
+        <TextField label="Pesquisar">
+          <Input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        </TextField>
+      </Search>
+    </Container>
+  );
+};
 
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.colors.primary};
+export const Container = styled.aside`
+  background-color: ${(props) => props.theme.colors.background};
+  width: 360px;
+  height: 100vh;
+  overflow-y: auto;
 `;
 
-export default Container;
+export const Search = styled.section`
+  display: flex;
+  background-color: #ffffff;
+  flex-direction: column;
+  justify-content: center;
+  padding: 1rem;
+`;
